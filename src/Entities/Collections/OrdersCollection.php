7f1b84c7612a1319b91719183d3bb0e12990607f
Samir Khattar <samir.khattar@edayna.com>
2020-05-15 14:40:24 +0300

    adding toArray() on OrderCollection

--- src/Entities/Collections/OrdersCollection.php
+++ src/Entities/Collections/OrdersCollection.php
@@ -7,4 +7,15 @@ use \Illuminate\Support\Collection;
 
 class OrdersCollection extends Collection implements OrdersCollectionContract
 {
+	/**
+	 * Get the collection of items as a plain array.
+	 *
+	 * @return array
+	 */
+	public function toArray()
+	{
+	    return array_map(function ($value) {
+	        return method_exists($value, 'toArray') ? $value->toArray() : $value;
+	    }, $this->items);
+	}
 }
