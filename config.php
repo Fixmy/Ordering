cde1e6f5f2a44409674617f005284afbf6a4c495
Samir Khattar <samir.khattar@edayna.com>
2020-05-15 14:41:29 +0300

    adding aliases for polymorphic bindings

--- config.php
+++ config.php
@@ -3,6 +3,11 @@
 return [
 	'database_predecessor' => 'fixme_',
 	'merchant_delivery_field' => 'provides_delivery',
+	'polymorph_bindings' => [
+		'product' => 'App\Models\Items\Item',
+		'beneficiary' => 'App\Models\Users\Beneficiary',
+	],
+
 ];
 
 ?>
